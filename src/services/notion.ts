3dfac7765f6b095f9073a177dca48aa71caf5bac
obvoso <autumninmoon@gmail.com>
2024-07-21 22:10:46 +0900

    feat: 내일 할 거

--- src/services/notion.ts
+++ src/services/notion.ts
@@ -6,7 +6,10 @@ const dbID = process.env.NOTION_DB_ID
 const notion = new Client({
   auth: token,
 })
-
+/**
+ * todo: 받아온 id를 이용해서 notion에 있는 데이터를 get하는 함수
+ */
+const getPost = async (id: string) => {}
 const getAll = async () => {
   const data = await notion.databases.query({
     database_id: dbID,
