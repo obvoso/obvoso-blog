631c446a5d52490f30bf6130044b49ecf0a41c96
obvoso <autumninmoon@gmail.com>
2024-07-22 15:59:18 +0900

    feat: notion to md
     - notion to md 라이브러리를 사용해서 페이지 내에 있는 요소를 md로 변환

--- src/services/notion.ts
+++ src/services/notion.ts
@@ -1,4 +1,5 @@
 const { Client } = require("@notionhq/client")
+const { NotionToMarkdown } = require("notion-to-md")
 
 const token = process.env.NOTION_TOKEN
 const dbID = process.env.NOTION_DB_ID
@@ -6,11 +7,33 @@ const dbID = process.env.NOTION_DB_ID
 const notion = new Client({
   auth: token,
 })
+
+/**
+ * todo: notion에서 받아온 데이터를 markdown으로 변환하는 함수
+ * @param id
+ * @returns list of markdown blocks
+ */
+const n2m = new NotionToMarkdown({
+  notionClient: notion,
+  config: {
+    separateChildPage: true, // default: false
+  },
+})
+
+export const getNotionData = async (id: string) => {
+  const mdblocks = await n2m.pageToMarkdown(id)
+  return mdblocks
+}
+
 /**
  * todo: 받아온 id를 이용해서 notion에 있는 데이터를 get하는 함수
  */
-const getPost = async (id: string) => {}
-const getAll = async () => {
+export const getPost = async (id: string) => {
+  const data = await notion.pages.retrieve({ page_id: id })
+  return data
+}
+
+export const getAll = async () => {
   const data = await notion.databases.query({
     database_id: dbID,
     filter: {
@@ -24,8 +47,5 @@ const getAll = async () => {
       direction: "descending",
     },
   })
-
-  console.log(data)
+  return data
 }
-
-export default getAll
