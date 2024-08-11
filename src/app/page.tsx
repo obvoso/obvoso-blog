6b2d4220867c23acf6dfc810b7e93e56c312afda
obvoso <autumninmoon@gmail.com>
2024-08-11 20:52:48 +0900

    feat: tag navigation 정렬
    - xs 이하일 때 가로 정렬로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <Container maxWidth="lg">
       <Box
+        paddingY={6}
         sx={{
           display: "flex",
           flexDirection: { xs: "column", sm: "row" },
--- src/components/tagSection/TagNavigation.tsx
+++ src/components/tagSection/TagNavigation.tsx
@@ -1,12 +1,22 @@
 import { getAllTagsWithCategory } from "@/services/notion"
 import { CategoryTag, TagEnum } from "@/types/tags"
+import { Box } from "@mui/material"
 import Tag from "./Tag"
 
 export default async function TagNavigation() {
   const data: CategoryTag[] = await getAllTagsWithCategory()
 
   return (
-    <div>
+    <Box
+      padding={4}
+      sx={{
+        display: "flex",
+        alignItems: "start",
+        flexDirection: { xs: "row", sm: "column" },
+        overflowX: { xs: "auto", sm: "visible" },
+        gap: 2,
+      }}
+    >
       {data.map((category) => (
         <div key={category.name}>
           <Tag tagName={category.name} type={TagEnum.CATEGORY} />
@@ -15,6 +25,6 @@ export default async function TagNavigation() {
           ))}
         </div>
       ))}
-    </div>
+    </Box>
   )
 }
