9c30b5e279252d53cfb2586a967b77155cf05e03
obvoso <autumninmoon@gmail.com>
2024-08-11 19:57:43 +0900

    refactor: ArticleThumbnail 컴포넌트 RCC -> RSC
    - ArticleThumbnail 컴포넌트를 RSC로 전환
    - 클라이언트 측 tag 상태 기반의 조건부 렌더링은 FilterArticle rcc에서 처리하도록 로직 변경

--- src/components/home/article/ArticleThumbnail.tsx
+++ src/components/home/article/ArticleThumbnail.tsx
@@ -1,52 +1,22 @@
-"use client"
+// "use client"
 
-import { selectTagsState } from "@/atoms/selectCategoryTags"
+import BlurImage from "@/components/common/BlurImage"
 import { NotionData } from "@/types/notion"
-import { TagEnum } from "@/types/tags"
 import { Box, Typography } from "@mui/material"
-import Image from "next/image"
-import { useRecoilValue } from "recoil"
 
 type ArticleProps = {
   article: NotionData
 }
 
 export default function ArticleThumbnail({ article }: ArticleProps) {
-  const tag = useRecoilValue(selectTagsState)
-  if (
-    tag.tagName !== "전체보기" &&
-    ((tag.type === TagEnum.TAG && !article.tag.includes(tag.tagName)) ||
-      (tag.type === TagEnum.CATEGORY && article.category !== tag.tagName))
-  )
-    return null
-
   return (
     <Box padding={4}>
       <Typography variant="h5">{article.title}</Typography>
       <Typography variant="body1">{article.description}</Typography>
-      <Box
-        sx={{
-          position: "relative",
-          width: "100%",
-          paddingBottom: "56.26%",
-          backgroundColor: "var(--primary)",
-        }}
-      >
-        <Image
-          src={article.thumbnail!}
-          alt="thumbnail"
-          placeholder={"blur"}
-          blurDataURL={article.blurThumbnail!}
-          width={0}
-          height={0}
-          sizes="100vw"
-          fill
-          style={{
-            borderRadius: "10px",
-            objectFit: "cover",
-          }}
-        />
-      </Box>
+      <BlurImage
+        src={article.thumbnail!}
+        blurDataURL={article.blurThumbnail!}
+      />
     </Box>
   )
 }
--- src/components/home/article/ArticlesList.tsx
+++ src/components/home/article/ArticlesList.tsx
@@ -1,21 +1,14 @@
 import { getAllPost } from "@/services/notion"
 import { NotionData } from "@/types/notion"
 import { Grid } from "@mui/material"
-import Link from "next/link"
-import ArticleThumbnail from "./ArticleThumbnail"
+import FilterArticle from "./FilterArticle"
 
 export default async function ArticlesList() {
   const data: NotionData[] = await getAllPost()
 
   return (
     <Grid container spacing={2}>
-      {data.map((post) => (
-        <Grid item xs={12} md={6} key={post.id}>
-          <Link href={`/articles/${post.slug}`}>
-            <ArticleThumbnail article={post} />
-          </Link>
-        </Grid>
-      ))}
+      <FilterArticle articles={data} />
     </Grid>
   )
 }
