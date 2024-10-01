929f8c36f38123cc10ebc46c83fdc60f4a061357
obvoso <autumninmoon@gmail.com>
2024-10-01 21:26:20 +0900

    design: footer layout
    - 푸터 레이아웃 위치 잡아놨습니다

---
+++ src/app/components/layout/Footer.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material"
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        marginTop: 4,
+        width: "100%",
+        height: "100px",
+        backgroundColor: "orange",
+      }}
+    >
+      bottom
+    </Box>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import RecoilRootWrapper from "@/app/RecoilWrapper"
 import { notoSansKr, notoSerifKr } from "@/styles/font"
 import "@/styles/globals.css"
 import "@/styles/themes.css"
+import { Box } from "@mui/material"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 import MobileHeader from "./(home)/profileSection/mobile/MobileHeader"
+import Footer from "./components/layout/Footer"
 
 export const metadata: Metadata = {
   title: "obvoso blog",
@@ -20,10 +22,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${notoSansKr.className}, ${notoSerifKr.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system">
-          <RecoilRootWrapper>
-            <MobileHeader />
-            <main>{children}</main>
-          </RecoilRootWrapper>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              minHeight: "100vh",
+            }}
+          >
+            <RecoilRootWrapper>
+              <MobileHeader />
+              <main>{children}</main>
+              <Footer />
+            </RecoilRootWrapper>
+          </Box>
         </ThemeProvider>
       </body>
     </html>
