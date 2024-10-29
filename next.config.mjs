482c22e43dd23725db12158563e1d26bc839fb29
soo <97217713+obvoso@users.noreply.github.com>
2024-10-29 13:45:04 +0900

    Update next.config.mjs
    - connect-src에 도메인 추가
    - 여전히 /_next/image에 대해 x-vercel-error: INVALID_IMAGE_OPTIMIZE_REQUEST 400이 날라옴..

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const devWs = isDev ? " ws://localhost:3000" : ""
 
 const cspHeader = `
   default-src 'self' https:${devHttp};
-  connect-src 'self' https:${devHttp}${devWs};
+  connect-src 'self' https://www.obvoso.site https:${devHttp}${devWs};
   img-src 'self' https://*.s3.amazonaws.com https://www.obvoso.site/_next/image data: blob: https:${devHttp};
   font-src 'self' data: https:${devHttp};
   style-src 'self' 'unsafe-inline' https:${devHttp};
